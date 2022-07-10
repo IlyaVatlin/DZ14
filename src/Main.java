5e3f908f7cea96814bb1dd2c94e3016c7238f5aa
IlyaVatlin <107053785+IlyaVatlin@users.noreply.github.com>
2022-07-10 19:03:22 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        BonusMilesService service = new BonusMilesService();
+        int price = 10000;
+        int bonus = service.calculate(price);
+        System.out.println(bonus);
+    }
 }
