966cdd1cc8bdcf9807fdf6ad788f6c9547d8eaaa
IlyaVatlin <107053785+IlyaVatlin@users.noreply.github.com>
2022-07-10 19:03:06 +0300

    Update BonusMilesService.java

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class BonusMilesService {
+public class BonusMilesService {
+    public int calculate(int price) {
+        int percent = 20;
+        int bonus = price / percent;
+        return bonus;
+    }
 }
